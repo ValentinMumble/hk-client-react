0f3119b6ff6d8244215ad207c83db59027d52b45
valentinmumble <valentindijkstra@gmail.com>
2019-09-10 11:30:01 +0200

    Fix swipeable drawers layout

--- src/Hues.js
+++ src/Hues.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react'
 import './Hues.css'
+import { IconButton } from '@material-ui/core'
+import { PowerSettingsNewRounded } from '@material-ui/icons'
 
 class Hues extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      colors: ['transparent', props.theme.palette.primary.main, '#ffffff', '#ffaa71', '#01a7c2', '#ff96ca']
+      colors: [props.theme.palette.primary.main, '#ffffff', '#ffaa71', '#01a7c2', '#ff96ca']
     }
   }
   componentDidUpdate(prevProps) {
     if (this.props.theme !== prevProps.theme) {
       const colors = Array.from(this.state.colors)
-      colors[1] = this.props.theme.palette.primary.main
+      colors[0] = this.props.theme.palette.primary.main
       this.setState({ colors })
     }
   }
   render() {
     return (
       <div className="Hues">
+        <IconButton color="inherit" onClick={() => this.props.onHueClick()}>
+          <PowerSettingsNewRounded />
+        </IconButton>
         {this.state.colors.map((color, i) => <div key={i} style={{ backgroundColor: color }} onClick={() => this.props.onHueClick(color)}></div>)}
       </div>
     )
