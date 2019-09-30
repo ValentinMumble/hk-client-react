31f1de8f719ba448918051b1f0383b5eef4ecdf8
Valentin Dijkstra <valentindijkstra@gmail.com>
2019-09-30 22:29:49 +0200

    Clean up splashy, use async/await syntax, fix no artwork bug

--- src/Artwork.js
+++ src/Artwork.js
@@ -10,36 +10,28 @@ class Artwork extends Component {
       prev: I.BLACK,
       progress: props.progress
     };
-    this.img = React.createRef();
+    this.imgRef = React.createRef();
   }
   componentDidMount() {
-    this.loadArtwork(this.props.src);
+    this.loadArtwork();
     this.progressTimer = window.setInterval(() => {
       if (this.props.isPlaying) this.setState({ progress: this.state.progress + 200 });
     }, 200);
   }
   componentDidUpdate(prevProps) {
-    if (this.props.src !== prevProps.src) this.loadArtwork(this.props.src);
+    if (this.props.src !== prevProps.src) this.loadArtwork();
     if (this.props.trackDuration !== prevProps.trackDuration) this.setState({ progress: 0 });
     if (this.props.progress !== prevProps.progress) this.setState({ progress: this.props.progress });
   }
-  loadArtwork = url => {
-    if (url) {
-      fetchImage(url, this.onArtworkData);
-    } else {
-      this.onArtworkData(I.GRAY);
-    }
-  };
-  onArtworkData = async (data, image) => {
-    // TODO clean this & fix bug when no artwork
-    this.setState({ current: data, hidden: 'Hidden' });
-    this.props.onColorChange(await splashy(this.img.current));
-    setTimeout(() => this.setState({ prev: data, hidden: '' }), 600);
+  loadArtwork = async () => {
+    this.setState({ current: this.props.src ? await fetchImage(this.props.src) : I.GRAY, hidden: 'Hidden' });
+    this.props.onColorChange(this.props.src ? await splashy(this.imgRef.current) : ['#777', '#777']);
+    setTimeout(() => this.setState({ prev: this.state.current, hidden: '' }), 600);
   };
   render() {
     return (
       <div className={`Artwork ${this.props.isPlaying ? '' : 'paused'}`} onClick={this.props.onClick}>
-        <img ref={this.img} src={this.state.current} alt='' />
+        <img ref={this.imgRef} src={this.state.current} alt='' />
         <img className={this.state.hidden} src={this.state.prev} alt='' />
         <div
           className='Progress'
--- src/util.js
+++ src/util.js
@@ -14,16 +14,21 @@ export const api = async (uri, { data = {}, method = 'GET' } = {}) => {
   return json;
 };
 
-export const fetchImage = (url, callback) => {
-  const downloadedImg = new Image();
-  downloadedImg.crossOrigin = 'Anonymous';
-  downloadedImg.src = url;
-  downloadedImg.addEventListener('load', () => {
-    const canvas = document.createElement('canvas');
-    canvas.width = downloadedImg.width;
-    canvas.height = downloadedImg.height;
-    canvas.getContext('2d').drawImage(downloadedImg, 0, 0);
-    callback(canvas.toDataURL('image/png'), downloadedImg);
+export const fetchImage = url => {
+  return new Promise((resolve, reject) => {
+    const newImg = new Image();
+    newImg.crossOrigin = 'Anonymous';
+    newImg.src = url;
+    newImg.addEventListener('load', () => {
+      const canvas = document.createElement('canvas');
+      canvas.width = newImg.width;
+      canvas.height = newImg.height;
+      canvas.getContext('2d').drawImage(newImg, 0, 0);
+      resolve(canvas.toDataURL('image/png'));
+    });
+    newImg.addEventListener('error', () => {
+      reject(new Error(`Failed to load image's URL: ${url}`));
+    });
   });
 };
 
